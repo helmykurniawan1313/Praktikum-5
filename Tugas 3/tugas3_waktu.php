ff442b2f79eb9bb4a83d321bbb6226f3bc97d580
helmykurniawan1313 <61194161+helmykurniawan1313@users.noreply.github.com>
2020-03-10 20:30:44 +0700

    Delete tugas3_waktu.php

--- Tugas 3/tugas3_waktu.php
+++
@@ -1,5 +0,0 @@
-<?php
-		date_default_timezone_set("asia/jakarta");
-		echo "<center> Tanggal Input : ".date("D, d-m-Y")."<center><br>";
-		echo "<center> Waktu Input : ".date("h:i:s")."<center><br>";
-?>
