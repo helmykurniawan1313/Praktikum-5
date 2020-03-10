beb822e70a603b6878f778bae224ddb5b9b29df2
helmykurniawan1313 <61194161+helmykurniawan1313@users.noreply.github.com>
2020-03-10 18:11:58 +0700

    Add files via upload

---
+++ Praktikum 5 PHP/Praktikum 5 (Array).php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<title>Pemrograman PHP dengan Array</title>
+</head>
+<body>
+<?php
+	$nama[] = "Helmy";
+	$nama[] = "Yudha";
+	$nama[] = "Arrandi";
+	echo $nama[1].$nama[2].$nama[0];
+	echo "<br>";
+?>
+</body>
+</html>
---
+++ Praktikum 5 PHP/Praktikum 5 (test).php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Refresh" content="2; url=http://localhost/Praktikum 5/test.html"> 
+<title>Untitled Document</title>
+</head>
+
+<body>
+	Tes Redirect
+</body>
+</html>
